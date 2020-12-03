f0d8e253f6a362a044fbd1480bf109936abe3697
Zeb Turp <163636@chrome.escg.ac.uk>
2020-12-03 09:29:49 +0000

    Pacman Game Added
    Added the game with the work I have done up until now

---
+++ Assets/Scripts/LoseLifePac.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseLifePac : MonoBehaviour
+{
+    public GameObject pacman;
+    public GameObject life1;
+    public GameObject life2;
+    public GameObject life3;
+    public GameObject ghost1;
+    Vector3 startPos;
+    Vector3 ghost1startPos;
+    int i = 0;
+
+    void Start()
+    {
+         startPos = this.transform.position;
+         ghost1startPos = ghost1.transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            this.transform.position = startPos;
+            ghost1.transform.position = ghost1startPos;
+            i = i + 1;
+            if (i == 1)
+            {
+                Debug.Log("You lost a life1");
+                Destroy(life3);
+            }
+            else if (i == 2)
+            {
+                Debug.Log("You lost a life2");
+                Destroy(life2);
+            }
+            else if (i == 3)
+            {
+                Debug.Log("You lost a life3");
+                Destroy(life1);
+            }
+            else if (i == 4)
+            {
+                Debug.Log("You lost");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 15);
+            player.transform.rotation = Quaternion.identity;
+            player.transform.Rotate(0.0f, -90.0f, 0.0f);
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            player.GetComponent<Rigidbody>().velocity = new Vector3(-15, 0, 0);
+            player.transform.rotation = Quaternion.identity;
+            player.transform.Rotate(0.0f, 0.0f, 180.0f);
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -15);
+            player.transform.rotation = Quaternion.identity;
+            player.transform.Rotate(0.0f, 90.0f, 0.0f);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            player.GetComponent<Rigidbody>().velocity = new Vector3(15, 0, 0);
+            player.transform.rotation = Quaternion.identity;
+        }
+    }
+}
